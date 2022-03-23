8bac69ebcaa322239815f70001a897d1c1a9ea27
Arabian-Mikayla <77825928+Arabian-Mikayla@users.noreply.github.com>
2022-03-23 11:17:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include "money.h"
+#include "account.h"
+
+int main(){
+
+  Account deposit(200,00);
+  Account deposit(300,24);
+  Account deposit(501,22);
+  Account withdrawal(300,10);
+  Account withdrawal(201,34);
+  
+  Money m(100,10);
+  Money 
+
+  
+  return 0;
+}
