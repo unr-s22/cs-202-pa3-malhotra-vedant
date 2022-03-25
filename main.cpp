984f95ae271ca80d3bb24cd34e10c8bba2758191
Arabian-Mikayla <77825928+Arabian-Mikayla@users.noreply.github.com>
2022-03-25 15:34:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ int main(){
   acc.makeDeposit(Money(300, 24));
   acc.makeDeposit(Money(501, 22));
   std::cout << acc << std::endl;
+  //deposit sum is $1301.69
 
   acc.makeWithdrawal(Money(300, 10));
   acc.makeWithdrawal(Money(201, 34));
   std::cout << acc << std::endl;
+  //amount after withdrawals are $800.25 
   
   return 0;
 }
