ceb15f204e7e939cc06a9a0895db38cfba826bdd
bunnies <bunnies@LaptopMK.localdomain>
2022-03-25 14:52:01 -0700

    :)

--- main.cpp
+++ main.cpp
@@ -3,29 +3,17 @@
 #include "account.h"
 
 int main(){
+  
+  Money m(100,10);
+  Account acc(300, 23);
 
-/*
   Account deposit(200,00);
   Account deposit(300,24);
   Account deposit(501,22);
   Account withdrawal(300,10);
   Account withdrawal(201,34);
-*/  
-  Money m(100,10);
-  Account acc(300, 23);
 
 std::cout << m << std::endl;
-
-//std::cout << "Account Details" << std::endl;
-//std::cout << "------------------------" << std::endl;
-//std::cout << "Current balance: " << balance << std::endl;
-//std::cout << "------------------------" << std::endl;
-//std::cout << "Number of Deposits: " << ndeposit << std::endl;
-//std::cout << "------------------------" << std::endl;
-//std::cout << "Number of Withdrawals: " << nwithdrawal << std::endl;
-//std::cout << "------------------------" << std::endl;
-//std::cout << "New balance: " << updated << std::endl;
-  //Account acc()
   
   return 0;
 }
