16fbb81519d94f4c1515f016f164a6d39d39461a
1103-Malhotra-Vedant <vmalhotra@nevada.unr.edu>
2022-03-25 14:50:44 -0700

    prints account balance

--- main.cpp
+++ main.cpp
@@ -4,17 +4,9 @@
 
 int main(){
 
-/*
-  Account deposit(200,00);
-  Account deposit(300,24);
-  Account deposit(501,22);
-  Account withdrawal(300,10);
-  Account withdrawal(201,34);
-*/  
-  Money m(100,10);
-  Account acc(300, 23);
+  Account acc(300,23);
+  std::cout << acc << std::endl;
 
-std::cout << m << std::endl;
 
 //std::cout << "Account Details" << std::endl;
 //std::cout << "------------------------" << std::endl;
