d96d7b4cc594116e9cb34ff877e9183ecdddc23a
1103-Malhotra-Vedant <vmalhotra@nevada.unr.edu>
2022-03-25 13:46:12 -0700

    started work on account

--- account.h
+++ account.h
@@ -5,16 +5,18 @@
 #include <vector>
 #include "money.h"
 
-class Account {
+class Account: public Money {
 
   private:
     std::vector<Money> moneyVe;
     bool moneyFlag = false;
 
   public:
+    explicit Account(Money acc);
     void makeDeposit(Money mon);
     void makeWithdrawal(Money mon);
     void updateAmount();
+    friend std::ostream &operator<<(std::ostream &os, const Account &acc);
 
   //explicit Account(Money mon(int a, int b));
 
