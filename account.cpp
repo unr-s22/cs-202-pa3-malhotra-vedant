425a04728d536f8d78f0f3e0068447d5d01cf49f
Arabian-Mikayla <bunnies@LaptopMK.localdomain>
2022-03-28 17:09:47 -0700

    removed setprecision notes

--- account.cpp
+++ account.cpp
@@ -14,8 +14,6 @@ void Account::makeWithdrawal(Money mon){
 
 std::ostream &operator<<(std::ostream &os, const Account &acc){
 
-    //setprecision? with needed header, other ways to do it as well. have websites and such to fix decimal problem
-
     Money initial = acc;
     if (acc.moneyFlag == true){
         for(int count = 0; count < acc.moneyDep.size(); count++) {
@@ -53,6 +51,6 @@ std::ostream &operator<<(std::ostream &os, const Account &acc){
     //boolean false by maybe doing something like this?
     //if(acc.moneyDep.size() = 0){return acc;}
     //if(acc.moneyWith.size() = 0){return acc;}
-    
+
     return os;
 }
